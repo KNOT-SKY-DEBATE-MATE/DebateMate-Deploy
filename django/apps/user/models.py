72165ff686b56221f2f19087cf84263826fd7114
kd1358122TakuyaKimura <kd1358122@st.kobedenshi.ac.jp>
2025-01-22 00:46:24 +0900

    djangoの修正

--- django/apps/user/models.py
+++ django/apps/user/models.py
@@ -1,5 +1,4 @@
 import uuid
-import nh3
 
 from django.db import models
 from django.contrib.auth.models import (
@@ -11,8 +10,3 @@ class User(AbstractUser):
 
     # User UUID Primary Key
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-
-    def save(self):
-        self.first_name = nh3.clean_text(self.first_name)
-        self.last_name = nh3.clean_text(self.last_name)
-        self.username = nh3.clean_text(self.username)
